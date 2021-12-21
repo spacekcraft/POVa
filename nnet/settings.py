259aed75d4d478377858b08f46e2bf66fbd1d8bb
Ján Pavlus <spacekcraft@gmail.com>
2021-12-21 20:03:48 +0100

    Update alphabet

--- nnet/settings.py
+++ nnet/settings.py
@@ -7,7 +7,7 @@ PERO_DATASET_PATH = os.environ.get('PERO_DATASET_PATH', "./pero/lines")
 PERO_ANNOTATIONS_PATH_TRAIN = os.environ.get('PERO_ANNOTATIONS_TRAIN', "./pero/train.easy")
 PERO_ANNOTATIONS_PATH_VAL = os.environ.get('PERO_ANNOTATIONS_VAL', "./pero/val.easy")
 
-ALPHABET = string.ascii_letters + string.digits + string.punctuation + ' °§ěščřžýáíéĚŠČŘŽÝÁÍÉďĎťŤňŇ'
+ALPHABET = string.ascii_letters + string.digits + string.punctuation + ' ©°§ěščřžýáíéĚŠČŘŽÝÁÍÉďĎťŤňŇ'
 NUMBER_OF_CLASSES = len(ALPHABET)
 
 TRAINING_PROGRESS_LOG_INTERVAL = 100
