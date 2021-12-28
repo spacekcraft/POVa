a6bd657508facd144359d41e54326299d7a8a270
minosalko <minosalko@gmail.com>
2021-12-28 16:54:33 +0100

    Uprava abecedy

--- nnet/settings.py
+++ nnet/settings.py
@@ -7,7 +7,9 @@ PERO_DATASET_PATH = os.environ.get('PERO_DATASET_PATH', "./pero/lines")
 PERO_ANNOTATIONS_PATH_TRAIN = os.environ.get('PERO_ANNOTATIONS_TRAIN', "./pero/train.easy")
 PERO_ANNOTATIONS_PATH_VAL = os.environ.get('PERO_ANNOTATIONS_VAL', "./pero/val.easy")
 
-ALPHABET = string.ascii_letters + string.digits + string.punctuation + ' ©°§ěščřžýáíéĚŠČŘŽÝÁÍÉďĎťŤňŇ'
+#ALPHABET = string.ascii_letters + string.digits + string.punctuation + ' ©°§ěščřžýáíéĚŠČŘŽÝÁÍÉďĎťŤňŇ'
+
+ALPHABET = ' !"#$%\'()*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]_abcdefghijklmnopqrstuvwxyz{|}~§©°é'
 NUMBER_OF_CLASSES = len(ALPHABET)
 
 TRAINING_PROGRESS_LOG_INTERVAL = 100
